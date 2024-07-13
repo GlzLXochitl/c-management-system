3ba28715e8b94e6137d90bd635fe3d4e30f78727
glzlxochitl <xochitlgl4903@gmail.com>
2024-07-13 16:57:02 -0600

    commit

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -6,24 +6,26 @@ export function Header() {
     return (
         <header>  
                <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                    <a className="navbar-brand" href="#!" id="logotype">
+                    <Link to="/userDashboard"  className="navbar-brand" id="logotype">
                         <div className="logotype">
                             <div>
                                 <img src="/images/utmaLogotype.png" width="38px" height="40px" alt="Logotype" />
                             </div>
                         </div>
-                    </a>
+                    </Link>
 
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                    <div class="links collapse navbar-collapse" id="navbarsExampleDefault">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item"><Link to="/userDashboard" className="nav-link UserDashboard">Inicio</Link></li>
                             <li class="nav-item"><Link to="/profile" className="nav-link UserDashboard">Perfil</Link></li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#!" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Configuración</a>
+                                <a class="nav-link dropdown-toggle" href="#!" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <img src="/images/config.png" width="20px" height="20px" alt="Logotype" />
+                                </a>
                                 <div class="dropdown-menu menu" aria-labelledby="navbarDropdownMenuLink">     
                                     
                                     {/*<h6 className="dropdown-header title-section-menu">Preferencias de Usuario</h6>     
